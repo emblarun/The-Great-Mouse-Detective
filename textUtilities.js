0607069a8ed6634609657f4acf46266da72568e6
Embla Rún <emblarungun@gmail.com>
2018-03-06 22:22:31 +0000

    Expect to be a string

---
+++ textUtilities.js
@@ -0,0 +1,14 @@
+var expect = require("chai").expect;
+
+
+// expect(true).to.be.true;
+
+titleCase = (title) => {
+  var words = title.split(" ");
+  var titleCasedWords = words.map(function (word) {
+    return word[0].toUpperCase() + word.substring(1);
+  });
+  return titleCasedWords.join(" ");
+}
+
+expect(titleCase("the great mouse detective")).to.be.a("string");
